9eae2b59418b9fc176d79260c30f86eaedee3a05
Max <mthoma67@students.byron.k12.mn.us>
2026-01-28 18:28:50 -0600

    Everything works, started building a pedro auto in PedroBlueShort

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/Pedro/PedroBlueShort.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.opmode.auto.Pedro;
+
+public class PedroBlueShort {
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/Tuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/Tuning.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.opmode.teleop;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.subsystems.TurretSubsystem;
@@ -8,7 +9,7 @@ import org.firstinspires.ftc.teamcode.hardware.Robot;
 
 //import org.firstinspires.ftc.teamcode.SimpleLedController;
 
-// @Disabled
+@Disabled
 @TeleOp
 
 
